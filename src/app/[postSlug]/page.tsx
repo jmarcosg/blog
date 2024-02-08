5da2ca0513e9c972d4039c147230f087b5c6d72e
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-08 13:18:19 -0300

    ✨ add breadcrumbs and header colors

--- src/app/[postSlug]/page.tsx
+++ src/app/[postSlug]/page.tsx
@@ -1,11 +1,10 @@
 import NotionRenderer from "@/components/notion-renderer";
-import { Badge } from "@/components/ui";
+import { Badge, Button } from "@/components/ui";
 import { filterPublishedPosts, getAllPosts } from "@/lib/notion";
 import { getPostBlocks } from "@/lib/notion/get-post-blocks";
 import { formatDate } from "@/utils";
+import Link from "next/link";
 import { notFound } from "next/navigation";
-
-const timeToRead = 3;
 
 export async function generateStaticParams() {
 	const posts = await getAllPosts({ includePages: false });
@@ -23,6 +22,7 @@ async function getPost(postSlug: string) {
 	if (filteredPosts.length === 0) return { post: null, recordMap: null };
 	const post = filteredPosts[0];
 	const recordMap = await getPostBlocks(post.id);
+
 	return { post, recordMap };
 }
 
@@ -41,11 +41,24 @@ const PostPage = async (props: PostPageProps) => {
 
 	return (
 		<div className="mt-20">
+			<div className="bg-[#fbe2e3] absolute top-[-20rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
+			<div className="bg-[#dbd7fb] absolute top-[-15rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
+
+			<div className="flex flex-wrap gap-1 mb-4">
+				<Button variant="link" className="dark:text-white" asChild>
+					<Link href={"/"}>Home</Link>
+				</Button>
+				<Button variant="link" className="dark:text-white">
+					/
+				</Button>
+				<Button variant="link" className="dark:text-white" asChild>
+					<Link href={"#"}>{post.title}</Link>
+				</Button>
+			</div>
+
 			<h1 className="mb-4 text-4xl font-bold">{post.title}</h1>
 			<div className="flex items-center space-x-2">
 				<p>Published in {formatDate(post.createdTime)}</p>
-				<p>•</p>
-				<p>{timeToRead} min read</p>
 				<p>•</p>
 				<div className="flex space-x-1">
 					{post.tags?.map((tag) => (
