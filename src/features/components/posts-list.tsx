6c8e289896c3ad453e8e94988628ff097b47a034
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-08 11:13:51 -0300

    🎨 format code

--- src/features/components/posts-list.tsx
+++ src/features/components/posts-list.tsx
@@ -1,26 +1,26 @@
-import { TPost } from "@/types"
-import { PostListItem } from "./post-list-item"
+import { TPost } from "@/types";
+import { PostListItem } from "./post-list-item";
 
 interface PostsListProps {
-  posts: TPost[]
+	posts: TPost[];
 }
 
 export const PostsList = (props: PostsListProps) => {
-  const { posts } = props
-  return (
-    <ul className="flex flex-col space-y-4">
-      {posts.map((post) => (
-        <PostListItem
-          key={post.slug}
-          href={`/blog/${post.slug}`}
-          title={post.title}
-          createdTime={post.createdTime}
-          timeToRead={4}
-          summary={post.summary}
-          thumbnail={post?.thumbnail}
-          tags={post.tags}
-        />
-      ))}
-    </ul>
-  )
-}
+	const { posts } = props;
+	return (
+		<ul className="flex flex-col space-y-4">
+			{posts.map((post) => (
+				<PostListItem
+					key={post.slug}
+					href={`/${post.slug}`}
+					title={post.title}
+					createdTime={post.createdTime}
+					timeToRead={4}
+					summary={post.summary}
+					thumbnail={post?.thumbnail}
+					tags={post.tags}
+				/>
+			))}
+		</ul>
+	);
+};
