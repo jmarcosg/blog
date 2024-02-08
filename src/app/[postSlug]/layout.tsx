47ecf27a9635ce2684d311bba4ef6535294cb04f
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-08 13:17:53 -0300

    ✨ simplify layout

--- src/app/[postSlug]/layout.tsx
+++ src/app/[postSlug]/layout.tsx
@@ -8,14 +8,7 @@ export const revalidate = 3600;
 
 const PostLayout = (props: PostLayoutProps) => {
 	const { children } = props;
-	return (
-		<div>
-			<div className="bg-[#fbe2e3] absolute top-[-20rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
-			<div className="bg-[#dbd7fb] absolute top-[-15rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
-
-			<div className="container max-w-4xl">{children}</div>
-		</div>
-	);
+	return <div className="container max-w-4xl">{children}</div>;
 };
 
 export default PostLayout;
