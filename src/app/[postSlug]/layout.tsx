5d21d691d716c3703a9ac6a2ca047d2fea66a34f
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-08 17:15:04 -0300

    💄 make post layout coherent with main layout

--- src/app/[postSlug]/layout.tsx
+++ src/app/[postSlug]/layout.tsx
@@ -8,7 +8,26 @@ export const revalidate = 3600;
 
 const PostLayout = (props: PostLayoutProps) => {
 	const { children } = props;
-	return <div className="container max-w-4xl">{children}</div>;
+	return (
+		<>
+			<svg
+				className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light"
+				width="100%"
+				height="100%"
+			>
+				<filter id="grainynoise">
+					<feTurbulence
+						type="fractalNoise"
+						baseFrequency="0.80"
+						numOctaves="4"
+						stitchTiles="stitch"
+					/>
+				</filter>
+				<rect width="100%" height="100%" filter="url(#grainynoise)" />
+			</svg>
+			<div className="container max-w-4xl">{children}</div>
+		</>
+	);
 };
 
 export default PostLayout;
--- src/app/[postSlug]/page.tsx
+++ src/app/[postSlug]/page.tsx
@@ -40,7 +40,7 @@ const PostPage = async (props: PostPageProps) => {
 	if (!post) notFound();
 
 	return (
-		<div className="mt-20">
+		<div className="pt-8">
 			<div className="bg-[#fbe2e3] absolute top-[-20rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
 			<div className="bg-[#dbd7fb] absolute top-[-15rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
 
