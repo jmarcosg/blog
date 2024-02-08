e86d1eb30d70b0b50949bbb305c4d68c06c91fdf
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-08 13:16:38 -0300

    🎨 format exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,9 +1,7 @@
-export * from "./badge"
-export * from "./button"
-export * from "./command"
-export * from "./dialog"
-export * from "./input"
-export * from "./separator"
-export * from "./sheet"
-export * from "./use-toast"
-
+export * from "./badge";
+export * from "./button";
+export * from "./command";
+export * from "./dialog";
+export * from "./input";
+export * from "./separator";
+export * from "./sheet";
