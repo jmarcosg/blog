772e5053f3469a9b822d9bd8f0949a0054837592
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-08 17:20:19 -0300

    🩹 fix noisy background making navbar bigger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
 			<body>
 				<Analytics />
 				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+					<svg
+						className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light"
+						width="100%"
+						height="100%"
+					>
+						<filter id="grainynoise">
+							<feTurbulence
+								type="fractalNoise"
+								baseFrequency="0.80"
+								numOctaves="4"
+								stitchTiles="stitch"
+							/>
+						</filter>
+						<rect width="100%" height="100%" filter="url(#grainynoise)" />
+					</svg>
 					<Header />
 					<main className="min-h-[calc(100dvh-64px)]">{children}</main>
 					<Footer />
