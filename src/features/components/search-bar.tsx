52135a33334a9e031e7a8a6a8b3f10bf9a167821
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-07 21:12:38 -0300

    ✨ improve search bar

--- src/features/components/search-bar.tsx
+++ src/features/components/search-bar.tsx
@@ -1,47 +1,44 @@
-"use client"
+"use client";
 
-import { Search } from "@/assets/icons"
-import { Input } from "@/components/ui"
-import React from "react"
+import { Search } from "@/assets/icons";
+import { Input } from "@/components/ui";
+import React from "react";
 
 interface SearchBarProps {
-  value: string
-  onChange: React.ChangeEventHandler<HTMLInputElement>
+	value: string;
+	onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 export const SearchBar = (props: SearchBarProps) => {
-  const { value, onChange } = props
+	const { value, onChange } = props;
 
-  const inputRef = React.useRef<HTMLInputElement | null>(null)
+	const inputRef = React.useRef<HTMLInputElement | null>(null);
 
-  React.useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.ctrlKey && e.key === "k" && inputRef.current) {
-        e.preventDefault()
-        inputRef.current.focus()
-      }
-    }
+	React.useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if ((e.metaKey || e.ctrlKey) && e.key === "k" && inputRef.current) {
+				e.preventDefault();
+				inputRef.current.focus();
+			}
+		};
 
-    document.addEventListener("keydown", handleKeyDown)
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown)
-    }
-  }, [])
+		document.addEventListener("keydown", handleKeyDown);
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
 
-  return (
-    <div className="relative w-full">
-      <Search className="absolute inset-y-0 left-3 my-auto h-6 w-6 text-gray-500" />
-      <Input
-        type="search"
-        value={value}
-        onChange={onChange}
-        placeholder="Search a post, a topic, a keyword..."
-        className="rounded-2xl px-12 py-7"
-        ref={inputRef}
-      />
-      <span className="absolute inset-y-0 right-8 z-[-1] my-auto h-6 w-6 text-gray-500">
-        ctrl+k
-      </span>
-    </div>
-  )
-}
+	return (
+		<div className="relative w-full">
+			<Search className="absolute inset-y-0 left-3 my-auto h-6 w-6 text-gray-500" />
+			<Input
+				type="search"
+				value={value}
+				onChange={onChange}
+				placeholder="Search a post, a topic, a keyword..."
+				className="rounded-2xl px-12 py-7 bg-neutral-100/40 dark:bg-slate-700 transition-all text-gray-500 dark:text-gray-100"
+				ref={inputRef}
+			/>
+		</div>
+	);
+};
