880a3da42173fc5eb66e83ecf58ddede3b2a7381
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-02-09 13:25:08 -0300

    ✨ improve suspense ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui";
 import { PostsExplorer, getAllTags } from "@/features";
 import { filterPublishedPosts, getAllPosts } from "@/lib/notion";
 import { generateRssFeed } from "@/utils/rss";
@@ -28,7 +29,33 @@ const Blog = async () => {
 			<div className="bg-[#dbd7fb] absolute top-[-15rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
 			<h1 className="mb-4 text-center	text-4xl font-bold">Hello World!</h1>
 
-			<Suspense fallback={<div>Loading...</div>}>
+			<Suspense
+				fallback={
+					<>
+						<div className="container mb-10 max-w-3xl">
+							<div className="relative w-full">
+								<Skeleton className="absolute inset-y-0 left-3 my-auto h-6 w-6 text-gray-500" />
+								<Skeleton className="rounded-2xl px-12 py-7 bg-neutral-100/40 dark:bg-slate-700 transition-all text-gray-500 dark:text-gray-100" />
+							</div>
+						</div>
+
+						<div className="container mb-6 max-w-6xl">
+							<h2 className="mb-5 text-xl font-bold">Search blog by topics</h2>
+							<div className="flex flex-wrap gap-2">
+								<Skeleton className="w-20 h-8" />
+								<Skeleton className="w-20 h-8" />
+								<Skeleton className="w-20 h-8" />
+							</div>
+						</div>
+
+						<ul className="flex flex-col space-y-4">
+							<Skeleton className="w-full h-5" />
+							<Skeleton className="w-full h-8" />
+							<Skeleton className="w-full h-40" />
+						</ul>
+					</>
+				}
+			>
 				<PostsExplorer posts={posts} tags={tags} />
 			</Suspense>
 		</div>
